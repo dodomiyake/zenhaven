fa730b247d979fc71687122c2e18b1b51d1605c3
Oluwadamilola Ajayi <oluwadamilola.william@gmail.com>
2025-09-22 12:50:33 +0100

    stripe latest version

--- backend/src/routes/verify-payment.ts
+++ backend/src/routes/verify-payment.ts
@@ -8,7 +8,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2025-05-28.basil',
+    apiVersion: "2025-07-30.basil" as Stripe.LatestApiVersion,
 });
 
 router.post('/verify-payment', async (req, res) => {
